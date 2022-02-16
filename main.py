e0dee84749ea6684a3f466483da4b2e5c455f923
Diego De Morais <79227339+DiegoBolonik@users.noreply.github.com>
2022-02-15 23:42:07 -0300

    Update and rename main2.py to main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-import subprocess
 import sys
 import datetime
 import sqlite3
@@ -7,28 +5,34 @@ import time
 import shutil
 
 from PyQt6 import QtWidgets
-from PySide2.QtWidgets import QProgressBar, QApplication, QDialog, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QDialog, QApplication, QFileDialog
 from PyQt6.uic import loadUi
 
-# ------------DATA BASE CONFIG ------------------------
+
+# ----------------DATA BASE CONFIG ------------------------
 
 date = str(datetime.date.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S'))
 
-args ="t1.py"
 
 class Login(QDialog):
     def __init__(self):
-        super(Login, self).__init__()
-        loadUi("login.ui", self)
+        super(Login,self).__init__()
+        loadUi("login.ui",self)
         self.loginbutton.clicked.connect(self.loginfunction)
         self.password.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
         self.createaccbutton.clicked.connect(self.gotocreate)
         self.uploader.clicked.connect(self.browsefiles)
         self.send.clicked.connect(self.sendfiles)
 
+
     def browsefiles(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '/')
+        fname=QFileDialog.getOpenFileName(self, 'Open file', '/')
         self.filename.setText(fname[0])
+
+    def sendfiles(self):
+        source = self.filename.text()
+        shutil.copy(source, './upload')
+        print("Upload realizado com sucesso!!")
 
     def loginfunction(self):
         connection = sqlite3.connect('database.db')
@@ -47,17 +51,11 @@ class Login(QDialog):
         else:
             print("Dados de login incorretos!")
 
+
     def gotocreate(self):
         createacc = CreateAcc()
         widget.addWidget(createacc)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
-
-    def sendfiles(self):
-        source = self.filename.text()
-        shutil.copy(source, './upload')
-        print("Upload realizado com sucesso!!")
-        self.mainwindow()
-
+        widget.setCurrentIndex(widget.currentIndex()+1)
 
 class CreateAcc(QDialog):
     def __init__(self):
@@ -71,13 +69,13 @@ class CreateAcc(QDialog):
         connection = sqlite3.connect('database.db')
         c = connection.cursor()
         email = self.email.text()
-        if self.password.text() == self.confirmpass.text():
-            password = self.password.text()
-            login = Login()
+        if self.password.text( )==self.confirmpass.text():
+            password =self.password.text()
+            login =Login()
             widget.addWidget(login)
-            widget.setCurrentIndex(widget.currentIndex() + 1)
+            widget.setCurrentIndex(widget.currentIndex() +1)
             c.execute('INSERT INTO dados (login, password, datestamp) VALUES (?, ?, ?)',
-                      (email, password, date))
+                  (email, password, date))
             connection.commit()
             print("Successfully created acc with email: ", email, "and password: ", password)
             connection.close()
